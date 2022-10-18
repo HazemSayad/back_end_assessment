93acc839e7a90428a488c31a07dc855f668f8809
HazemSayad <94578938+HazemSayad@users.noreply.github.com>
2022-10-18 19:24:49 +0300

    created responses constants

---
+++ constants/responses.js
@@ -0,0 +1,13 @@
+module.exports = {
+  InvalidEmail: "InvalidEmail",
+  InvalidPassword: "InvalidPassword",
+  AccountNotFound: "No account found with the email",
+  InternalError: "InternalError",
+  Success: "Success",
+  AccountAlreadyExists: "AccountAlreadyExists",
+  Unauthorized: "Unauthorized",
+  TokenExpiredError: "TokenExpiredError",
+  InvalidProductName: "InvalidProductName",
+  InvalidProductDescription: "InvalidProductDescription",
+  InvalidProductId: "InvalidProductId",
+};
